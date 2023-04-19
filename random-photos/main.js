eb53ba5e57a0d7558b370125095e5ab786484033
sujata karki <sujatakarki018@gmail.com>
2023-04-19 19:27:53 +0545

    initial commit

--- random-photos/main.js
+++ random-photos/main.js
@@ -1,1 +1,9 @@
 const imageContainerEl = document.querySelector(".container");
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click",()=>{
+    addNewImages();
+})
+function addNewImages(){
+    
+}
